8fadaaa1e08d02d764cfc5fe231e33f560dc3029
AlexandreTedesco <alexandre.tedesco75@gmail.com>
2024-11-14 09:01:55 +0100

    log

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -4,6 +4,7 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
+    console.log(`Test: should foo - Expected name: "foo", Actual name: ${items[0].name}`);
     expect(items[0].name).toEqual("foo");
   });
 
@@ -11,12 +12,14 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     gildedRose.updateQuality();
     const items = gildedRose.items;
+    console.log(`Test: should update the quality of foo correctly - Expected quality: 0, Actual quality: ${items[0].quality}`);
     expect(items[0].quality).toBe(0);
   });
 
   it("should keep the name of foo unchanged", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
+    console.log(`Test: should keep the name of foo unchanged - Expected name: "foo", Actual name: ${items[0].name}`);
     expect(items[0].name).toBe("foo");
   });
 
@@ -24,6 +27,7 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("Normal Item", 5, 10)]);
     gildedRose.updateQuality();
     const items = gildedRose.items;
+    console.log(`Test: should decrease quality by 1 for normal items - Expected quality: 9, Actual quality: ${items[0].quality}`);
     expect(items[0].quality).toBe(9);
   });
 
@@ -31,6 +35,7 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
     gildedRose.updateQuality();
     const items = gildedRose.items;
+    console.log(`Test: should increase quality by 1 for Aged Brie - Expected quality: 1, Actual quality: ${items[0].quality}`);
     expect(items[0].quality).toBe(1);
   });
 
@@ -38,6 +43,7 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("Aged Brie", 2, 50)]);
     gildedRose.updateQuality();
     const items = gildedRose.items;
+    console.log(`Test: should not increase quality above 50 - Expected quality: 50, Actual quality: ${items[0].quality}`);
     expect(items[0].quality).toBe(50);
   });
 
@@ -45,6 +51,7 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("Conjured Mana Cake", 3, 6)]);
     gildedRose.updateQuality();
     const items = gildedRose.items;
+    console.log(`Test: should decrease quality by 2 for Conjured items - Expected quality: 4, Actual quality: ${items[0].quality}`);
     expect(items[0].quality).toBe(4);
   });
 
@@ -52,6 +59,7 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("Conjured Mana Cake", 0, 6)]);
     gildedRose.updateQuality();
     const items = gildedRose.items;
+    console.log(`Test: should decrease quality by 2 for Conjured items after sellIn - Expected quality: 2, Actual quality: ${items[0].quality}`);
     expect(items[0].quality).toBe(2);
   });
 
@@ -59,6 +67,7 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
     gildedRose.updateQuality();
     const items = gildedRose.items;
+    console.log(`Test: should not change quality of Sulfuras - Expected quality: 80, Actual quality: ${items[0].quality}`);
     expect(items[0].quality).toBe(80);
   });
 
@@ -66,6 +75,7 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 40)]);
     gildedRose.updateQuality();
     const items = gildedRose.items;
+    console.log(`Test: should drop quality to 0 for Backstage passes after the concert - Expected quality: 0, Actual quality: ${items[0].quality}`);
     expect(items[0].quality).toBe(0);
   });
 
@@ -73,6 +83,7 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)]);
     gildedRose.updateQuality();
     const items = gildedRose.items;
+    console.log(`Test: should increase quality by 3 for Backstage passes when 5 days or less - Expected quality: 43, Actual quality: ${items[0].quality}`);
     expect(items[0].quality).toBe(43);
   });
 
